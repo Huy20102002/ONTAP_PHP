41ead9c3c7041fa5762f25361c619dcf955971a6
Huy20102002 <97575301+Huy20102002@users.noreply.github.com>
2022-07-14 09:17:07 +0700

    Add files via upload

---
+++ index2.php
@@ -0,0 +1,26 @@
+<?php
+// Session
+// session_start();
+// if(isset($_SESSION['counter'])){
+//     $_SESSION['counter']+=1;
+// }else{
+//     $_SESSION['counter']=1;
+// }
+// echo "Số lần truy cập" . $_SESSION['counter'] . 'Lần vào trang';
+
+// Xóa session
+// sử dụng unset để xóa session mong muốn
+// Example: delete session['counter']
+// unset($_SESSION['counter']);
+// Sử dụng session_Destroy để xóa toàn bộ session
+// session_destroy();
+
+// Cookie
+
+// Sự khác biệt giữa session và cookie
+// 1. Session: chỉ có thể tồn tại đến khi người dùng đóng trình duyệt, mỗi client sẽ có sessionId khác nhau nên không
+// bao giờ xảy ra xung đột session. Session được lưu trong biến toàn cục $_SESSION
+// 2. Cookie: Cookie thì được lưu trữ ở máy của client nên khi đóng trình duyệt thì cookie vẫn tồn tại, thời gian của cookie là do 
+// người dùng set
+
+
